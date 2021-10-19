52431634a6d919399db9baa3908bf852387643b5
Jackson Capper <jacksoncapper@gmail.com>
2021-10-19 21:42:14 +1000

    Update structuredClone.js

--- structuredClone.js
+++ structuredClone.js
@@ -1,22 +1,22 @@
 function structuredClone(source, functions = false, index = [], callback = (index, value) => undefined) => {
-		let value = null;
-		value = callback(index, source);
-		if(value === undefined)
-			if(Array.isArray(source)) {
-				value = [...source];
-				for(let i = 0; i < value.length; i++)
-					value[i] = ASTROGRAPH.structuredClone(value[i], functions, [...index, i], callback);
-			}
-			else if(source === null)
-				value = null;
-			else if(typeof source == 'object' && source.__proto__ == Object.prototype) {
-				value = Object.assign({}, source);
-				for(let name in value)
-					value[name] = ASTROGRAPH.structuredClone(value[name], functions, [...index, name], callback);
-			}
-			else if(typeof source == 'function')
-				value = functions ? source.toString() : source;
-			else
-				value = source;
-		return value;
-	}
+	let value = null;
+	value = callback(index, source);
+	if(value === undefined)
+		if(Array.isArray(source)) {
+			value = [...source];
+			for(let i = 0; i < value.length; i++)
+				value[i] = ASTROGRAPH.structuredClone(value[i], functions, [...index, i], callback);
+		}
+		else if(source === null)
+			value = null;
+		else if(typeof source == 'object' && source.__proto__ == Object.prototype) {
+			value = Object.assign({}, source);
+			for(let name in value)
+				value[name] = ASTROGRAPH.structuredClone(value[name], functions, [...index, name], callback);
+		}
+		else if(typeof source == 'function')
+			value = functions ? source.toString() : source;
+		else
+			value = source;
+	return value;
+}
